d93b40ba16fbdc00d2a41628a9dd77116dd99d85
Aqeela AlGhasra <aqeelah.alghasrah@gmail.com>
2024-12-24 20:04:25 +0300

    Find and Delete

--- server.js
+++ server.js
@@ -4,19 +4,16 @@ const dotenv = require('dotenv').config()
 require('./config/database.js')
 
 const express = require('express');
+const methodOverride = require("method-override"); // new
 const morgan = require('morgan');
-
+const Fruit =require('./models/fruit.js')
 const app = express();
 
 // models 
 
-require('./models/fruit.js')
-
-
-
-
-
 // MIDDLEWARE
+
+app.use(methodOverride("_method"));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 // ROUTES
@@ -28,17 +25,46 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
-  
+
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  // console.log(allFruits); // log the fruits!
+  // res.send("Welcome to the index page!");
+  res.render("fruits/index.ejs", { fruits: allFruits });
+});
 
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
 });
 
+app.get("/fruits/:fruitId", async (req,res)=>{
+    // const fruitId = req.params.fruitId;
+    // const fruit = await allFruits.find(fruitId);
+    // res.render('this fruit has this Id', fruitId);
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render("fruits/show.ejs", { fruit: foundFruit });
 
+  })
+app.delete("/fruits/:fruitId", async (req,res)=>{
+    
+    const foundFruit = await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+    
+
+  })
+// server.js
+
+// POST /fruits
 app.post("/fruits", async (req, res) => {
-  console.log(req.body);
-  res.redirect("/fruits/new");
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits");
 });
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
